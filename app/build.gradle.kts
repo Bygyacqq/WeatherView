11cc80689dfc35a5d913e29b0ae7e1e426f3c7d2
Dayana Pulido <144375079+Bygyacqq@users.noreply.github.com>
2024-05-18 12:51:34 -0500

    WeatherView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.github.MatteoBattilana:WeatherView:3.0.0")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("androidx.annotation:annotation-jvm:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/hotel/example/weatherview/MainActivity.java
+++ app/src/main/java/hotel/example/weatherview/MainActivity.java
@@ -1,14 +1,71 @@
 package hotel.example.weatherview;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "WeatherView";
+
+    private static final String API_KEY = "Wview";
+    private static final String API_URL = "https://api.openweathermap.org/data/2.5/forecast?q=%s&units=metric&appid=" + API_KEY;
+
+    private EditText searchEditText;
+
+    private RecyclerView recyclerView;
+
+    private WeatherAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapter = new WeatherAdapter(this, new ArrayList());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+
+    }
+
+    @Override
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_search) {
+
+            searchWeather();
+
+            return true;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void searchWeather() {
+        String location = searchEditText.getText().toString();
+    }
+
+    public class ArrayList {
     }
 }
